f73af95dd32a2134022a6c18ff58b433485935d3
kmasullo-fadv <k_masullo@outlook.com>
2022-12-11 09:53:18 -0500

    Navbar colors and items added

--- src/nav/NavComponent.tsx
+++ src/nav/NavComponent.tsx
@@ -1,21 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpotify, faBandcamp, faLinkedin, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import React from 'react'
+import React from 'react';
+import './NavComponent.css';
 
 const NavComponent = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid col-8">
-                <a className="navbar-brand col-4" href="#">Kameron Masullo</a>
+                <a className="navbar-brand col-5" href="#">Kameron Masullo</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse justify-content-between col-8" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse justify-content-between col-7" id="navbarSupportedContent">
                     <ul className="navbar-nav mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <li className="nav-item mx-1">
+                            <a className="nav-link active navMenuItem" aria-current="page" href="#">Home</a>
                         </li>
-                        <li className="nav-item dropdown">
+                        <li className="nav-item mx-1">
+                            <a className="nav-link active navMenuItem" aria-current="page" href="#">Dev</a>
+                        </li>
+                        <li className="nav-item mx-1">
+                            <a className="nav-link active navMenuItem" aria-current="page" href="#">Music</a>
+                        </li>
+                        <li className="nav-item mx-1">
+                            <a className="nav-link active navMenuItem" aria-current="page" href="#">Contact</a>
+                        </li>
+                        {/* <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown
                             </a>
@@ -25,31 +35,31 @@ const NavComponent = () => {
                                 <li><hr className="dropdown-divider" /></li>
                                 <li><a className="dropdown-item" href="#">Something else here</a></li>
                             </ul>
-                        </li>
+                        </li> */}
                     </ul>
-                    <ul className='navbar-nav mb-2 mb-lg-0'>
+                    <ul className='navbar-nav mb-2 mb-lg-0 navMenuItems'>
                         <li className="nav-item">
-                            <a className="nav-link" target='_blank' href="https://github.com/kmasullo-fadv">
+                            <a className="nav-link navMenuItem" target='_blank' href="https://github.com/kmasullo-fadv">
                                 <FontAwesomeIcon icon={faGithub} size='2x' />
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" target='_blank' href="https://linkedin.com/in/kameron-masullo">
+                            <a className="nav-link navMenuItem" target='_blank' href="https://linkedin.com/in/kameron-masullo">
                                 <FontAwesomeIcon icon={faLinkedin} size='2x' />
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" target='_blank' href="https://twitter.com/K_m_r_n">
+                            <a className="nav-link navMenuItem" target='_blank' href="https://twitter.com/K_m_r_n">
                                 <FontAwesomeIcon icon={faTwitter} size='2x' />
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" target='_blank' href="https://kameronmasullo.bandcamp.com/">
+                            <a className="nav-link navMenuItem" target='_blank' href="https://kameronmasullo.bandcamp.com/">
                                 <FontAwesomeIcon icon={faBandcamp} size='2x' />
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" target='_blank' href="https://open.spotify.com/artist/5G946pHaGg1bWL8UjiZPdB?si=D0iBu9EsTSO5Vr8t-2MYfQ">
+                            <a className="nav-link navMenuItem" target='_blank' href="https://open.spotify.com/artist/5G946pHaGg1bWL8UjiZPdB?si=D0iBu9EsTSO5Vr8t-2MYfQ">
                                 <FontAwesomeIcon icon={faSpotify} size='2x' />
                             </a>
                         </li>
