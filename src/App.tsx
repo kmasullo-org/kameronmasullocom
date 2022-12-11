d958cc7065783ddc9a9dba9998d85288a31e12b4
kmasullo-fadv <k_masullo@outlook.com>
2022-12-11 10:42:27 -0500

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Footer from './footer/Footer';
 import NavComponent from './nav/NavComponent';
 
 const App = () => {
-  return (
-    <NavComponent />
-  );
+    return (
+        <>
+            <NavComponent />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
